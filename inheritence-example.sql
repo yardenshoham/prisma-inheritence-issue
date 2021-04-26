d25ad50b43e0c934e9125c237e698581a1cb238c
yardenshoham <hrsi88@gmail.com>
2021-04-27 00:29:44 +0300

    Reproduce

---
+++ inheritence-example.sql
@@ -0,0 +1,10 @@
+-- https://www.postgresql.org/docs/current/tutorial-inheritance.html
+CREATE TABLE cities (
+  name       text,
+  population real,
+  elevation  int     -- (in ft)
+);
+
+CREATE TABLE capitals (
+  state      char(2) UNIQUE NOT NULL
+) INHERITS (cities);
